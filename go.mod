c2b5caa1e18f90adb7896f16e2aaf594aeb8089a
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-10-11 00:56:35 +0900

    add grpc mod

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/SuzukiTakamasa/Go-gRPC
 
 go 1.22.3
+
+require google.golang.org/grpc v1.67.1
+
+require (
+	github.com/golang/protobuf v1.5.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto v0.0.0-20210126160654-44e461bb6506 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
 func main() {
 	fmt.Println("This will be a sample gRPC server entrypoint.")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to listen on port %s: %v", port, err)
+	}
+
+	s := grpc.NewServer(
+		grpc.Creds(insecure.NewCredentials()),
+	)
+
+	log.Printf("gRPC server listening on port %s", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port %s: %v", port, err)
+	}
 }
